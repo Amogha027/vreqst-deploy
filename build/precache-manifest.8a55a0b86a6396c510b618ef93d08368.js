6cc604889ba0fb3860ae45906578601959a58ab3
Amogha027 <aahalhalli@gmail.com>
2024-05-24 16:15:56 +0530

    latest

---
+++ build/precache-manifest.8a55a0b86a6396c510b618ef93d08368.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea575b416346cecaf7f7b8de8407cd31",
+    "url": "/vreqst-deploy/index.html"
+  },
+  {
+    "revision": "747d12c3efc9fd27f849",
+    "url": "/vreqst-deploy/static/css/main.246c8ac2.chunk.css"
+  },
+  {
+    "revision": "88a8aa1840462708f03e",
+    "url": "/vreqst-deploy/static/js/2.beda2708.chunk.js"
+  },
+  {
+    "revision": "6b647c612c8d9845700141d8247a2c3c",
+    "url": "/vreqst-deploy/static/js/2.beda2708.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "747d12c3efc9fd27f849",
+    "url": "/vreqst-deploy/static/js/main.854aac4f.chunk.js"
+  },
+  {
+    "revision": "fd4a26861391f44278b3",
+    "url": "/vreqst-deploy/static/js/runtime-main.b8a7c6fe.js"
+  },
+  {
+    "revision": "fdb795e970c9485b3ee804de0d66f5b0",
+    "url": "/vreqst-deploy/static/media/action.fdb795e9.txt"
+  },
+  {
+    "revision": "a80fba56cbac9db56dccf9701952c9b2",
+    "url": "/vreqst-deploy/static/media/asset.a80fba56.txt"
+  },
+  {
+    "revision": "a7a93f0d2970fbfd93b97af6e5df74f4",
+    "url": "/vreqst-deploy/static/media/scene.a7a93f0d.txt"
+  },
+  {
+    "revision": "44f05b74bb5bea1ce5dd308de65ed12a",
+    "url": "/vreqst-deploy/static/media/timeline.44f05b74.txt"
+  }
+]);
